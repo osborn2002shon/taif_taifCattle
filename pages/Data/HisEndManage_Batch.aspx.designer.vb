63736d3eaf5a96aaf389d5fe6595c81d42562a72
osborn2002shon <30406280+osborn2002shon@users.noreply.github.com>
2025-10-29 17:48:36 +0800

    Implement cattle removal batch import

--- pages/Data/HisEndManage_Batch.aspx.designer.vb
+++ pages/Data/HisEndManage_Batch.aspx.designer.vb
@@ -1,4 +1,4 @@
-﻿'------------------------------------------------------------------------------
+'------------------------------------------------------------------------------
 ' <自動產生>
 '     這段程式碼是由工具產生的。
 '
@@ -12,4 +12,103 @@ Option Explicit On
 
 
 Partial Public Class HisEndManage_Batch
+
+    '''<summary>
+    '''FileUpload_excel 控制項。
+    '''</summary>
+    '''<remarks>
+    '''自動產生的欄位。
+    '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+    '''</remarks>
+    Protected WithEvents FileUpload_excel As Global.System.Web.UI.WebControls.FileUpload
+
+    '''<summary>
+    '''LinkButton_import 控制項。
+    '''</summary>
+    '''<remarks>
+    '''自動產生的欄位。
+    '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+    '''</remarks>
+    Protected WithEvents LinkButton_import As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''HyperLink_template 控制項。
+    '''</summary>
+    '''<remarks>
+    '''自動產生的欄位。
+    '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+    '''</remarks>
+    Protected WithEvents HyperLink_template As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''LinkButton_downloadFailed 控制項。
+    '''</summary>
+    '''<remarks>
+    '''自動產生的欄位。
+    '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+    '''</remarks>
+    Protected WithEvents LinkButton_downloadFailed As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''Label_message 控制項。
+    '''</summary>
+    '''<remarks>
+    '''自動產生的欄位。
+    '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+    '''</remarks>
+    Protected WithEvents Label_message As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Panel_success 控制項。
+    '''</summary>
+    '''<remarks>
+    '''自動產生的欄位。
+    '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+    '''</remarks>
+    Protected WithEvents Panel_success As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''Label_successCount 控制項。
+    '''</summary>
+    '''<remarks>
+    '''自動產生的欄位。
+    '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+    '''</remarks>
+    Protected WithEvents Label_successCount As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''GridView_success 控制項。
+    '''</summary>
+    '''<remarks>
+    '''自動產生的欄位。
+    '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+    '''</remarks>
+    Protected WithEvents GridView_success As Global.System.Web.UI.WebControls.GridView
+
+    '''<summary>
+    '''Panel_failed 控制項。
+    '''</summary>
+    '''<remarks>
+    '''自動產生的欄位。
+    '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+    '''</remarks>
+    Protected WithEvents Panel_failed As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''Label_failedCount 控制項。
+    '''</summary>
+    '''<remarks>
+    '''自動產生的欄位。
+    '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+    '''</remarks>
+    Protected WithEvents Label_failedCount As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''GridView_failed 控制項。
+    '''</summary>
+    '''<remarks>
+    '''自動產生的欄位。
+    '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+    '''</remarks>
+    Protected WithEvents GridView_failed As Global.System.Web.UI.WebControls.GridView
 End Class
